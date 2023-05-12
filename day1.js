ac4da4e4b09fbf32c5f2840fd91cb5992005ae09
Kenya Orozco <kenyaorozco31@gmail.com>
2023-05-11 23:59:56 -0500

    first commit

---
+++ day1.js
@@ -0,0 +1,26 @@
+function acronmize(str){}
+
+var str1 = "object oriented programming";
+var expected1 = "OOP";
+
+result = str1[0]
+for( var i = 0; i < str1.length; i++){
+    if (str1[i] === " "){
+    result = str1[0] + [i]
+    }
+}
+console.log(result(str1));
+
+
+
+var str2 = "abrastaction polymorphism inheritance encapsulation";
+var expected2 = "APIE";
+
+return str2 = 0
+for ( var b=0; b < str2.length;b++){
+    if(str2[b] === " "){
+        return = str2[0]+ [i]
+    }
+}
+
+console.log(acronymize(str2))
